ceca3a58e7189df7c23edccdb0f88295dde34556
Controlled Chaos Design <greg@ccdzine.com>
2019-10-06 19:24:19 -0700

    Initial commit

---
+++ template-parts/content-page.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @package    WordPress/ClassicPress
+ * @subpackage BS_Theme
+ * @since      1.0.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
+	<header class="entry-header">
+		<?php
+		if ( is_front_page() ) {
+
+			// Front page uses h1 in the page header so h2 here.
+			the_title( '<h2 class="entry-title">', '</h2>' );
+		} else {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} ?>
+	</header>
+
+	<?php BS_Theme\Tags\post_thumbnail(); ?>
+
+	<div class="entry-content" itemprop="articleBody">
+		<?php
+		the_content();
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bs-theme' ),
+			'after'  => '</div>',
+		) );
+		?>
+	</div>
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'bs-theme' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</footer>
+	<?php endif; ?>
+</article>
